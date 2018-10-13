b7a1b07593744c2b2e47f84f63de01dcd2e97b84
Adam Pod <adam.podoxin@gmail.com>
2018-10-12 19:20:10 -0700

    Created win conditions

--- TicTacToe_AI/TicTacToe_AI/Form1.cs
+++ TicTacToe_AI/TicTacToe_AI/Form1.cs
@@ -42,6 +42,19 @@ namespace TicTacToe_AI
                 grid[xPos, yPos] = currentMark;
                 SwitchMark();
                 turns++;
+                CheckForWin();
+            }
+        }
+
+        public void Mark(int xPos, int yPos, Button button)
+        {
+            if (grid[xPos, yPos].Equals(""))
+            {
+                grid[xPos, yPos] = currentMark;
+                button.Text = currentMark;
+                SwitchMark();
+                turns++;
+                CheckForWin();
             }
         }
 
@@ -57,58 +70,90 @@ namespace TicTacToe_AI
             }
         }
 
+        public void CheckForWin()
+        {
+            //All horizontal wins
+            for (int y = 0; y < 3; y++)
+            {
+                if (grid[0, y] != "" && grid[0, y] == grid[1, y] && grid[1, y] == grid[2, y])
+                {
+                    Win(currentMark);
+                    return;
+                }
+            }
+
+            //All vertical wins
+            for (int x = 0; x < 3; x++)
+            {
+                if (grid[x, 0] != "" && grid[x, 0] == grid[x, 1] && grid[x, 1] == grid[x, 2])
+                {
+                    Win(currentMark);
+                    return;
+                }
+            }
+
+            //All diagonal wins
+            if (grid[0, 0] != "" && grid[0, 0] == grid[1, 1] && grid[1, 1] == grid[2, 2])
+            {
+                Win(currentMark);
+                return;
+            }
+            if (grid[0, 2] != "" && grid[0, 2] == grid[1, 1] && grid[1, 1] == grid[2, 0])
+            {
+                Win(currentMark);
+                return;
+            }
+        }
+
+        public void Win(string mark)
+        {
+            Console.WriteLine(mark + " wins");
+            Application.Exit();
+        }
+
         private void top_left_Click(object sender, EventArgs e)
         {
-            Mark(0, 0);
-            top_left.Text = currentMark;
+            Mark(0, 0, top_left);
         }
 
         private void top_center_Click(object sender, EventArgs e)
         {
-            Mark(1, 0);
-            top_center.Text = currentMark;
+            Mark(1, 0, top_center);
         }
 
         private void top_right_Click(object sender, EventArgs e)
         {
-            Mark(2, 0);
-            top_right.Text = currentMark;
+            Mark(2, 0, top_right);
         }
 
         private void center_left_Click(object sender, EventArgs e)
         {
-            Mark(0, 1);
-            center_left.Text = currentMark;
+            Mark(0, 1, center_left);
         }
 
         private void center_center_Click(object sender, EventArgs e)
         {
-            Mark(1, 1);
-            center_center.Text = currentMark;
+            Mark(1, 1, center_center);
         }
 
         private void center_right_Click(object sender, EventArgs e)
         {
-            Mark(2, 1);
-            center_right.Text = currentMark;
+            Mark(2, 1, center_right);
         }
 
         private void bottom_left_Click(object sender, EventArgs e)
         {
-            Mark(0, 2);
-            bottom_left.Text = currentMark;
+            Mark(0, 2, bottom_left);
         }
 
         private void bottom_center_Click(object sender, EventArgs e)
         {
-            Mark(1, 2);
-            bottom_center.Text = currentMark;
+            Mark(1, 2, bottom_center);
         }
 
         private void bottom_right_Click(object sender, EventArgs e)
         {
-            Mark(2, 2);
-            bottom_right.Text = currentMark;
+            Mark(2, 2, bottom_right);
         }
     }
 }
